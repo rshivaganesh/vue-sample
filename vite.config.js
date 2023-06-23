a765caa1627a1cdf350ad5923bc15b166950797c
rsganesh <rshivaganesh@gmail.com>
2023-06-23 22:56:15 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-sample/',
   plugins: [
     vue(),
   ],
@@ -14,3 +15,4 @@ export default defineConfig({
     }
   }
 })
+
